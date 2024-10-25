23c4c557d35e601432d376a989c2d0cad9955708
leorasaharia <saharialeora2406@gmail.com>
2024-10-25 14:17:42 +0530

    files added

---
+++ src/components/common/footer/Footer.jsx
@@ -0,0 +1,100 @@
+import React from "react"
+import { blog } from "../../../dummydata"
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <>
+    <section className='newletter'>
+      <div className='container flexSB'>
+        <div className='left row'>
+          <h1>Stay Updated with ARWellness</h1>
+          <span>Get the latest insights on VR-based wellness solutions for seniors</span>
+        </div>
+        <div className='right row'>
+          <input type='text' placeholder='Enter email address' />
+          <i className='fa fa-paper-plane'></i>
+        </div>
+      </div>
+    </section>
+      <footer>
+        <div className='container padding'>
+          <div className='box logo'>
+            <h1>ARWellness</h1>
+            <span>VR WELLNESS SOLUTIONS FOR SENIORS</span>
+            <p>Enhancing seniors' quality of life through VR-based physical therapy, cognitive training, and social connection.</p>
+
+            <i className='fab fa-facebook-f icon'></i>
+            <i className='fab fa-twitter icon'></i>
+            <i className='fab fa-instagram icon'></i>
+          </div>
+          <div className='box link'>
+            <h3>Explore</h3>
+            <ul>
+              <li>About Us</li>
+              <li>Services</li>
+              <li>Programs</li>
+              <li>Blog</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div className='box link'>
+            <h3>Quick Links</h3>
+            <ul>
+              <li>Contact Us</li>
+              <li>Pricing</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+              <li>Feedback</li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h3>Recent Posts</h3>
+            {blog.slice(0, 3).map((val) => (
+              <div className='items flexSB' key={val.id}>
+                <div className='img'>
+                  <img src={val.cover} alt='' />
+                </div>
+                <div className='text'>
+                  <span>
+                    <i className='fa fa-calendar-alt'></i>
+                    <label htmlFor=''>{val.date}</label>
+                  </span>
+                  <span>
+                    <i className='fa fa-user'></i>
+                    <label htmlFor=''>{val.type}</label>
+                  </span>
+                  <h4>{val.title.slice(0, 40)}...</h4>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className='box last'>
+            <h3>Have Questions?</h3>
+            <ul>
+              <li>
+                <i className='fa fa-map'></i>
+                123 Wellness St., Virtual World, Immersive City, VR
+              </li>
+              <li>
+                <i className='fa fa-phone-alt'></i>
+                +1 800 123 4567
+              </li>
+              <li>
+                <i className='fa fa-paper-plane'></i>
+                support@arwellness.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+      <div className='legal'>
+        <p>
+          Copyright ©2024 All rights reserved | This platform is developed with <i className='fa fa-heart'></i> by Team NotFound
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
